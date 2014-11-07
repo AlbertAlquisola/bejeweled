e9dcaba5c4e1e764c94192e20e40bf810a1c1c8b
albert <aalquiso@gmail.com>
2014-11-06 16:30:16 -0800

    create vanishing triples animation. working version

--- jsx/cell.js
+++ jsx/cell.js
@@ -4,8 +4,8 @@ app.Views.Cell = React.createClass({
   render: function() {
     var backgroundColor = {backgroundColor : this.props.cell.value}
     return (
-      <div className="cell" id={this.props.cell.position} style={backgroundColor}>
-      </div>
+        <div className="cell" id={this.props.cell.position} style={backgroundColor}>
+        </div>
     )
   }
 })
--- react/cell.js
+++ react/cell.js
@@ -4,8 +4,8 @@ app.Views.Cell = React.createClass({displayName: 'Cell',
   render: function() {
     var backgroundColor = {backgroundColor : this.props.cell.value}
     return (
-      React.DOM.div({className: "cell", id: this.props.cell.position, style: backgroundColor}
-      )
+        React.DOM.div({className: "cell", id: this.props.cell.position, style: backgroundColor}
+        )
     )
   }
 })
